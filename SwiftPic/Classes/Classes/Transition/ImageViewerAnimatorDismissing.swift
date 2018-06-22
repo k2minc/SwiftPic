634ece0588269b83cc11c7dac9917215d030ec27
Alex Brown <abrown252@gmail.com>
2018-06-22 10:12:11 +0100

    Added documentation for the transition

--- SwiftPic/Classes/Classes/Transition/ImageViewerAnimatorDismissing.swift
+++ SwiftPic/Classes/Classes/Transition/ImageViewerAnimatorDismissing.swift
@@ -9,20 +9,26 @@
 import Foundation
 import UIKit
 
-public class ImageViewerAnimatorDismissing: NSObject, UIViewControllerAnimatedTransitioning {
-    
+public class ImageViewerAnimatorDismissing: NSObject {
+    // The imageView we're animating from.
     let originImageView: UIImageView
     let animationTime: Double
     
+    // MARK: init.
     public init(originImageView: UIImageView, animationTime: Double) {
         self.originImageView = originImageView
         self.animationTime = animationTime
     }
+}
+
+// MARK: UIViewControllerAnimatedTransitioning
+extension ImageViewerAnimatorDismissing: UIViewControllerAnimatedTransitioning {
     
     public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return animationTime
     }
     
+    // MARK: Transition
     public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         let container = transitionContext.containerView
         guard
@@ -30,34 +36,45 @@ public class ImageViewerAnimatorDismissing: NSObject, UIViewControllerAnimatedTr
             let originImage = originImageView.image
             else { return }
         
+        // The absolute position of the origin imageView in it's parent
         let toFrame = originImageView.convert(originImageView.bounds, to: fromVC.view)
+        
+        // The destination frame for the imageView pulled from the context
         let targetFrame = transitionContext.initialFrame(for: fromVC)
         
+        // Set the origin image to hidden, gives the impression that we're animating the original image
         originImageView.isHidden = true
-        
+
+        // Calculate the origin frame for the new imageView
         let offsetY = fromVC.viewModel.currentImageView().frame.origin.y
         let offsetX: CGFloat = 0
         let width = originImage.size.width
         let height = originImage.size.height
+        // important to get the true size of the imahge
         let aspectRatio = height / width
+        // This is the actual position of the image when shown in the origin UIImageView.
         let yPos = (targetFrame.size.height / 2) - (targetFrame.width * aspectRatio / 2) + offsetY
         let originFrame = CGRect(x: offsetX, y: yPos, width: targetFrame.width, height: targetFrame.width * aspectRatio)
         
+        // Setup our new imageView for the transition
         let imageView = UIImageView(frame: originFrame)
         imageView.image = originImage
         imageView.contentMode = originImageView.contentMode
         imageView.clipsToBounds = true
         imageView.clearsContextBeforeDrawing = false
         
+        // Fade out background
         let fadeOutView = UIView(frame: targetFrame)
         fadeOutView.backgroundColor = .black
         let currentAlpha: CGFloat = fromVC.view.backgroundColor?.cgColor.alpha ?? 1
         fadeOutView.alpha = currentAlpha
         
+        // Add everything to our transition container.
         fromVC.view.isHidden = true
         container.addSubview(fadeOutView)
         container.addSubview(imageView)
         
+        // Animate the transition
         let duration = transitionDuration(using: transitionContext)
         UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
             imageView.frame = toFrame
--- SwiftPic/Classes/Classes/Transition/ImageViewerAnimatorPresenting.swift
+++ SwiftPic/Classes/Classes/Transition/ImageViewerAnimatorPresenting.swift
@@ -9,8 +9,7 @@
 import Foundation
 import AVKit
 
-public class ImageViewerAnimatorPresenting: NSObject, UIViewControllerAnimatedTransitioning {
-    
+public class ImageViewerAnimatorPresenting: NSObject {
     let originImageView: UIImageView
     let animationTime: Double
     
@@ -18,6 +17,10 @@ public class ImageViewerAnimatorPresenting: NSObject, UIViewControllerAnimatedTr
         self.originImageView = originImageView
         self.animationTime = animationTime
     }
+}
+
+// MARK: UIViewControllerAnimatedTransitioning
+extension ImageViewerAnimatorPresenting: UIViewControllerAnimatedTransitioning {
     
     public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return animationTime
@@ -32,8 +35,13 @@ public class ImageViewerAnimatorPresenting: NSObject, UIViewControllerAnimatedTr
             let originImage = originImageView.image
             else { return }
         
+        // The absolute position of the origin imageView in it's parent
         let fromFrame = originImageView.convert(originImageView.bounds, to: fromVc.view)
+        
+        // The destination frame for the imageView pulled from the context
         let targetFrame = transitionContext.finalFrame(for: toVc)
+        
+        // Set the origin image to hidden, gives the impression that we're animating the original image
         originImageView.isHidden = true
         
         toView.translatesAutoresizingMaskIntoConstraints = true
@@ -48,18 +56,22 @@ public class ImageViewerAnimatorPresenting: NSObject, UIViewControllerAnimatedTr
         let width = originImage.size.width
         let height = originImage.size.height
         
+        // Borrowing functionality from AVKit to calculate the frame of the image as it will appear in the target imageView.
         let targetImageViewSize = AVMakeRect(aspectRatio: CGSize(width: width, height: height), insideRect: targetFrame)
         
+        // Calculate the origin frame for the new imageView
         let imageView = UIImageView(frame: fromFrame)
         imageView.image = originImage
         imageView.contentMode = originImageView.contentMode
         imageView.clipsToBounds = true
         imageView.clearsContextBeforeDrawing = false
         
+        // Setup container
         container.addSubview(toView)
         container.addSubview(fadeInView)
         container.addSubview(imageView)
         
+        // Animate the transition
         let duration = transitionDuration(using: transitionContext)
         UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.9, options: .curveEaseOut, animations: {
             imageView.frame = targetImageViewSize
